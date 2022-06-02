9ba22e759809ed3d717e3133c8e0736493883045
Zaid Mughal <51900989+zaidMughal@users.noreply.github.com>
2022-06-02 21:53:08 +0500

    Update Derivative_Analyze.py

--- Derivative_Analyze/Derivative_Analyze.py
+++ Derivative_Analyze/Derivative_Analyze.py
@@ -1,25 +1,50 @@
-STOCH_UPPER_THRESH = 75
-STOCH_LOWER_THRESH = 25
-STOCH_THRESH = 7
+INTERVAL = "6h"
+SYMBOL = ["BTCUSDT","BNBUSDT", "SOLUSDT", "XRPUSDT","NEARUSDT","ROSEUSDT"]      #All currencies this bot will trade upon
+EMA_PERIOD = 60         #How many past candles will we see while calculating EMA
+TREND_RANGE = -30       #Past EMA of how many periods will we see to decide future trend
 
-#working on 6h, -30
-@schedule(interval="6h", symbol="ADAUSDT")
+'''
+INTERVAL = "12h"     #For testing
+SYMBOL = ["BTCUSDT","BNBUSDT", "SOLUSDT", "XRPUSDT","NEARUSDT","ROSEUSDT"]
+TREND_RANGE = -15'''
+
+#working on 6h, 60, -30
+#bnb PEAK AT 70
+#ADA peak at 60 (70 also works)
+#ETH peak at 50
+#BTC peak at 60
+
+def initialize(state):
+    state.num_curr = len(SYMBOL)    #Num of currencies
+    state.buy = dict()
+    for currency in SYMBOL:
+        state.buy[currency] = 0     #Have we bought this currency or not
+
+@schedule(interval=INTERVAL, symbol=SYMBOL)
 def run(state, data):
-    ema21 = data.ema(period=50, select="close")
+        
+    for currency in data:
+        crypto_data = data[currency]
 
-    buy= 1
-    for i in range(-1,-30,-1):
-        der = ema21[i]-ema21[i-1]
-        if der < 0:
-            buy = 0
-            break
-    if buy:
-        order_market_value(symbol=data.symbol, value=query_balance_free("USDT"))
-    
-    if (ema21[-1]-ema21[-2]) < 0:       #sell
-        order_market_amount(data.symbol, amount=-query_balance_free("ADA"))
+        ema = crypto_data.ema(period=EMA_PERIOD , select="close")
+        buy= 1
+        for i in range(-1,TREND_RANGE,-1):      #-1,-2,-3,...TREND_RANGE
+            der = ema[i]-ema[i-1]   #Since time period is constant, we don't need to divide by interval
+            if der < 0:             #Make sure ema is increasing from past TREND_RANGE candles
+                buy = 0
+                break
 
-    with PlotScope.group("my_group", data.symbol):
-        plot_line("der",ema21[-1]-ema21[-2],data.symbol)
-        plot_line("0",0,data.symbol)
-   
+        if (ema[-1] < ema[-2]) and (state.buy[crypto_data.symbol] == 1):       #sell
+            order_market_amount(crypto_data.symbol, amount=-query_balance_free(crypto_data.base))
+            state.buy[crypto_data.symbol] = 0
+
+        if buy and (state.buy[crypto_data.symbol] == 0):    #Buy if signaled and not already bought
+            value=query_balance_free("USDT")/state.num_curr
+            value = value if value >15 else 15
+            order_market_value(symbol=crypto_data.symbol, value=value)
+            state.buy[crypto_data.symbol] = 1
+        
+
+        with PlotScope.group("my_group", crypto_data.symbol):
+            plot_line("derivative_div",(ema[-1]-ema[-2])*100/crypto_data.close_last,crypto_data.symbol)
+            #Comparatively normalized graph for derivative
